3366e86975dbd9c12d4e02682d58ea5e8a3876c2
svidovich <samuel.vidovich@gmail.com>
2021-03-25 22:51:03 -0400

    Update sample code to use JDBC connection

--- spark-postgres-jdbc/spark/pyspark_code/sample.py
+++ spark-postgres-jdbc/spark/pyspark_code/sample.py
@@ -1,3 +1,5 @@
+import os
+
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 
@@ -6,17 +8,23 @@ spark_session = SparkSession.builder.getOrCreate()
 
 
 def main():
-    sample_dataframe = spark_context.parallelize(
-        [
-            {
-                'first_name': 'Jeff',
-                'last_name': 'Lebowski',
-                'nickname': 'The Dude',
-            }
-        ]
-    ).toDF()
+    database = 'spark_test'
+    database_user = os.environ['SPARK_DATABASE_USER']
+    database_password = os.environ['SPARK_DATABASE_PASSWORD']
 
-    sample_dataframe.show()
+    jdbc_dataframe = spark_session.read.format(
+        'jdbc'
+    ).option(
+        'url', f'jdbc:postgresql://database:5432/{database}'
+    ).option(
+        'dbtable', 'public.test_table'
+    ).option(
+        'user', database_user
+    ).option(
+        'password', database_password
+    ).load()
+
+    jdbc_dataframe.show()
 
 
 if __name__ == '__main__':
