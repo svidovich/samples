451f324589facbf58809f6c9b276660ead81c0cc
svidovich <samuel.vidovich@gmail.com>
2021-05-15 22:05:36 -0400

    Mild restructure for common code

--- pub-sub-faas/src/faas/faas_main.py
+++ pub-sub-faas/src/faas/faas_main.py
@@ -1,25 +1,4 @@
-import os
-import pika
 
-
-RABBITMQ_HOST = os.environ['RABBITMQ_HOST']
-
-rabbitmq_connection = None
-rabbitmq_channel = None
-
-def get_rabbitmq_connection():
-    global rabbitmq_connection
-    if rabbitmq_connection is None:
-        connection_parameters = pika.ConnectionParameters(RABBITMQ_HOST)
-        rabbitmq_connection = pika.BlockingConnection(connection_parameters)
-    return rabbitmq_connection
-
-
-def get_rabbitmq_channel():
-    global rabbitmq_channel
-    if rabbitmq_channel is None:
-        rabbitmq_channel = get_rabbitmq_connection().channel()
-    return rabbitmq_channel
 
 
 def main():
